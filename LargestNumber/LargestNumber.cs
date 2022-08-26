45dc4d8d24aa4dfab464b4a3c856a253f66da05a
sahib2442 <kadersahib1234@gmail.com>
2022-08-26 16:43:19 +0530

    [sahib] Add: largest number

---
+++ LargestNumber/LargestNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LargestNumber
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int large;
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter third number : ");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b && a > c)
+                large = a;
+            else if (b > a && b > c)
+                large = b;
+            else large = c;
+            Console.WriteLine("Using if-else...");
+            Console.WriteLine("Largest number is {0}", large);
+            large = (a > b && a > c) ? a : (b > a && b > c) ? b : c;
+            Console.WriteLine("Using ternary operator...");
+            Console.WriteLine("Largest number is {0}", large); 
+            Console.ReadLine();
+        }
+    }
+}
+
+        }
---
+++ LargestNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LargestNumber
+{
+    internal class program
+    {
+        static void main(string[]args)
+        {
+            program program= new program();
+            Console.WriteLine("enter the largest number");
+        }
+    }
+}
